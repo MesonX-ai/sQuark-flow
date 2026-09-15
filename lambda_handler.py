7aa2a13831f5918a6b387a2ba6ee40da3af097be
MesonX <mesonx@mesonxs-mini.lan>
2026-09-15 16:55:04 -0400

    Quick deploy: 2026-09-15 16:55:04

--- lambda_handler.py
+++ lambda_handler.py
@@ -3,10 +3,35 @@
 import sys
 from types import ModuleType
 
-# Create dummy modules for botocore.docs
-sys.modules['botocore.docs'] = ModuleType('botocore.docs')
-sys.modules['botocore.docs.docstring'] = ModuleType('botocore.docs.docstring')
-sys.modules['botocore.docs.utils'] = ModuleType('botocore.docs.utils')
+# Create dummy modules and classes for botocore.docs
+botocore_docs = ModuleType('botocore.docs')
+botocore_docs_docstring = ModuleType('botocore.docs.docstring')
+botocore_docs_utils = ModuleType('botocore.docs.utils')
+
+# Add empty classes that boto3 might try to import
+class DocstringDocumenter:
+    pass
+
+class ClientMethodDocstring(DocstringDocumenter):
+    pass
+
+class PaginatorDocstring(DocstringDocumenter):
+    pass
+
+class WaiterDocstring(DocstringDocumenter):
+    pass
+
+# Attach classes to modules
+botocore_docs_docstring.DocstringDocumenter = DocstringDocumenter
+botocore_docs_docstring.ClientMethodDocstring = ClientMethodDocstring
+botocore_docs_docstring.PaginatorDocstring = PaginatorDocstring
+botocore_docs_docstring.WaiterDocstring = WaiterDocstring
+botocore_docs_docstring.LazyLoadedDocstring = DocstringDocumenter
+
+# Register modules
+sys.modules['botocore.docs'] = botocore_docs
+sys.modules['botocore.docs.docstring'] = botocore_docs_docstring
+sys.modules['botocore.docs.utils'] = botocore_docs_utils
 
 from mangum import Mangum
 from app.main import app
